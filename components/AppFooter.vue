2de2238393157415fa806817cef9a1da28926017
wumacms <wumacms@qq.com>
2026-04-18 17:52:02 +0800

    style: 优化页脚二维码图片样式，添加内边距和白色背景

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -25,7 +25,7 @@
               <div>
                   <h4 class="text-white font-semibold mb-4">联系方式</h4>
                   <ul class="space-y-2 text-sm text-stone-400">
-                      <li><i class="fas fa-phone-alt w-5 text-orange-500"></i> 电话：18503878846 / 13215991477</li>
+                      <li><i class="fas fa-phone-alt w-5 text-orange-500"></i> 电话：18503878846 或 13215991477</li>
                       <li><i class="fas fa-envelope w-5 text-orange-500"></i> 邮箱：530051528@qq.com</li>
                       <li><i class="fas fa-globe w-5 text-orange-500"></i> 官网：www.berrymedical.com.cn</li>
                   </ul>
@@ -35,21 +35,21 @@
                   <h4 class="text-white font-semibold mb-4">官方微信</h4>
                   <div class="flex gap-2 justify-center">
                       <div class="text-center">
-                          <img :src="getImageUrl('/images/qrcode/gongzhonghao.png')" 
+                          <img :src="getImageUrl('/images/qrcode/gongzhonghao.jpg')" 
                                alt="微信公众号二维码" 
-                               class="w-16 h-16 rounded-lg border border-stone-700">
+                               class="w-16 h-16 rounded-lg border border-stone-700 p-1 bg-white">
                           <p class="mt-1 text-xs text-stone-500">公众号</p>
                       </div>
                       <div class="text-center">
                           <img :src="getImageUrl('/images/qrcode/shipinhao.png')" 
-                               alt="微信视频号二维码" 
-                               class="w-16 h-16 rounded-lg border border-stone-700">
+                               alt="微信二维码" 
+                               class="w-16 h-16 rounded-lg border border-stone-700 p-1 bg-white">
                           <p class="mt-1 text-xs text-stone-500">视频号</p>
                       </div>
                       <div class="text-center">
                           <img :src="getImageUrl('/images/qrcode/xiaochengxu.png')" 
                                alt="微信小程序二维码" 
-                               class="w-16 h-16 rounded-lg border border-stone-700">
+                               class="w-16 h-16 rounded-lg border border-stone-700 p-1 bg-white">
                           <p class="mt-1 text-xs text-stone-500">小程序</p>
                       </div>
                   </div>
