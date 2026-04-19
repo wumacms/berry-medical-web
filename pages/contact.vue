572daf2b63a059180026c3ef94a169330df5b599
wumacms <wumacms@qq.com>
2026-04-20 07:26:57 +0800

    fix: contact.vue 使用 getImageUrl 函数

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <PageHero
-      title="了解更多"
+      title="联系我们"
       subtitle="如果您对我们的产品或服务有任何疑问，请填写以下表单，我们将尽快与您联系。"
-      badge="联系我们"
+      badge=""
       badge-icon="fa-headset"
       :background-url="getImageUrl('/images/contact/cover.jpg')"
     />
